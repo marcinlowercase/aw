6ea2eb4d0b60a4c3b58e75d91a1fdd73b879cfe8
Theo <marcinlowercase@gmail.com>
2026-02-05 02:07:26 -0600

    zero

---
+++ ui/function/layout.js
@@ -0,0 +1,9 @@
+// layout.js
+
+// oo1.addEventListener("click", () => {
+//   window.location.href = "https://oo1.studio";
+// });
+
+// marcinlowercase.addEventListener("click", () => {
+//   window.location.href = "https://marcinlowercase.oo1.studio";
+// });
